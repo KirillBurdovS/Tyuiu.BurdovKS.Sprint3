2db3de9718c09838d8aded2d78dfb5702da06a10
Кирилл Бурдов <Tyuiu.BurdovKS@outlook.com>
2024-10-26 18:59:53 +0300

    Task2

---
+++ Tyuiu.BurdovKS.Sprint3.Task2.V12.Lib/DataService.cs
@@ -0,0 +1,41 @@
+﻿
+
+
+using System.Reflection.Metadata.Ecma335;
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+
+namespace Tyuiu.BurdovKS.Sprint3.Task2.V12.Lib
+{
+    public class DataService : ISprint3Task2V12
+    {
+        public double GetMultiplySeries(int value, int startValue, int stopValue)
+        {
+
+
+            double gg;
+
+            double wp;
+
+            double p = 1;
+
+            do
+            {
+
+                double m1 = startValue;
+
+                gg = m1 + Math.Pow(value, m1);
+
+                wp = 300 / gg;
+                
+                p *= Math.Pow(wp, m1);
+                startValue++;
+
+            }
+            while(startValue <= stopValue);
+
+            return Math.Round(p, 3);
+        }
+        
+    }
+}
---
+++ Tyuiu.BurdovKS.Sprint3.Task2.V12/Program.cs
@@ -0,0 +1,49 @@
+﻿
+
+
+using Tyuiu.BurdovKS.Sprint3.Task2.V12.Lib;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        int value = 5;
+        int startValue = 1;
+        int stopValue = 5;
+
+
+
+
+        Console.Title = "Спринт #3 | Выполнил: Бурдов.К.С | СМАРТБ-24-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Оператор цикла for                                                *");
+        Console.WriteLine("* Задание #2                                                              *");
+        Console.WriteLine("* Вариант #12                                                             *");
+        Console.WriteLine("* Выполнил: Бурдов Кирилл Сергеевич |  СМАРТБ-24-1                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу используя цикл do...while,                           *");
+        Console.WriteLine("*  которая вычисляет произведение ряда по формуле, при х=5                *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+
+
+
+        Console.WriteLine("Переменная X = " + value);
+        Console.WriteLine("Старт шага = " + startValue);
+        Console.WriteLine("Конец шага = " + stopValue);
+
+        Console.WriteLine("РЕЗУЛЬТАТ: ");
+
+        Console.WriteLine("Сумма ряда = " + ds.GetMultiplySeries(value, startValue, stopValue));
+
+        Console.ReadKey();
+    }
+}
